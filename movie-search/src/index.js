cf9a84f61b6cd8df5a14809bd824e8609ec8d197
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2020-04-28 18:03:48 +0300

    feat: add slider

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,3 +1,2 @@
 import './styles/index.scss'
-import '../index.html'
 import './scripts'
--- movie-search/webpack.config.js
+++ movie-search/webpack.config.js
@@ -1,38 +1,58 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: './'
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin([{
+      from: './src/assets',
+      to: './assets'
+    }])
+  ],
   module: {
     rules: [
       {
-        test: /.scss$/,
-        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       // images
       {
-        test: /.(png|jpg|svg)$/,
+        test: /\.(png|jpg|svg)$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/images/'
+              outputPath: './assets/images/'
             }
           }
         ]
       },
-      {
-        test: /\.html$/,
-        use: [
-          'file-loader?name=[path][name].[ext]',
-          'extract-loader',
-          'html-loader'
-        ]
-      },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     'file-loader?name=[path][name].[ext]',
+      //     'extract-loader',
+      //     {
+      //       loader: 'html-loader',
+      //       options: {
+
+      //         attributes: {
+      //           root: './'
+      //         }
+      //       }
+      //   },
+        // ],        
+      // },
       // fonts
       {
         test: /.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
